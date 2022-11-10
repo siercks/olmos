1165af7b60eb28042c87f8cb6839cd78c7f5c9af
siercks <asiercks@gmail.com>
2022-11-10 12:26:25 -0800

    new

---
+++ Olmos/MyApp.ServiceInterface/MyServices.cs
@@ -0,0 +1,12 @@
+using ServiceStack;
+using Olmos.ServiceModel;
+
+namespace Olmos.ServiceInterface;
+
+public class MyServices : Service
+{
+    public object Any(Hello request)
+    {
+        return new HelloResponse { Result = $"Hello, {request.Name}!" };
+    }
+}
